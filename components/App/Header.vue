d5bc888c011931e52c6a0d95ab090d529c519ed4
Renan Joppert <renanjoppert@outlook.com>
2023-10-14 10:50:07 -0300

    Header

---
+++ components/App/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="row items-center q-px-lg q-py-lg">
+    <div class="logo">
+      <img src="~/assets/img/logo-branca.png" height="75" />
+    </div>
+    <div class="navbar row items-center justify-center col-grow">
+      <q-btn flat color="white" label="Página Inicial" />
+      <q-btn flat color="white" label="Todos os Cursos" />
+      <q-btn flat color="white" label="Materiais" />
+      <q-btn flat color="white" label="Planos" />
+      <q-btn flat color="white" label="Sobre Nós" />
+      <q-btn flat color="white" label="Contato" />
+    </div>
+    <div class="cta-group row">
+      <q-btn text-color="primary" class="q-ma-sm" outline>
+        <AppIcon class="q-mr-sm" name="tabler:user" />Entrar
+      </q-btn>
+      <q-btn color="primary" text-color="black" class="q-ma-sm row">
+        <AppIcon class="q-mr-sm" name="jam:plus" />
+        Cadastre-se
+      </q-btn>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+
+
+<style>
+</style>
