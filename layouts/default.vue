a38916fc6dcc2f86dc4123c3dd45598109db970e
Renan Joppert <renanjoppert@outlook.com>
2023-10-14 11:05:38 -0300

    Background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-    <slot />
+  <div class="layout__default">
+    <div class="background-overlay min-h-screen bg-black/40">
+      <slot />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
 </script>
 
-
-
-<style>
-
+<style lang="scss">
+.layout__default {
+  background-image: url("~/assets/img/background.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+}
 </style>
