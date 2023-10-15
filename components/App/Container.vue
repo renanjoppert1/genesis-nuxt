c39a7b4be4602a171772280ae745bb17aa7b01f4
Renan Joppert <renanjoppert@outlook.com>
2023-10-15 00:51:37 -0300

    Componente de Container

---
+++ components/App/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="tw-container tw-flex tw-mx-auto tw-py-4 tw-box-border tw-px-4"
+    :class="{
+      'lg:tw-max-w-[1200px]': props.fluid === false,
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface ContainerProps {
+  fluid?: boolean;
+}
+
+const props = withDefaults(defineProps<ContainerProps>(), {
+  fluid: false,
+});
+</script>
+
+
+
+<style>
+</style>
