808151589690741b37fc85d6a2540cfd02360fe3
Renan Joppert <renanjoppert@outlook.com>
2023-10-14 23:47:50 -0300

    Dinamizando o menu

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -2,7 +2,7 @@
   <header
     class="tw-flex tw-flex-row tw-gap-4 md:tw-flex-row tw-items-center tw-px-5 tw-py-5"
   >
-    <div class="logo tw-w-1/2 tw-grow tw-flex tw-justify-center">
+    <div class="logo tw-flex tw-justify-center">
       <img
         src="~/assets/img/logo-branca.png"
         class="tw-h-[75px] tw-object-contain"
@@ -11,10 +11,16 @@
     <div
       class="navbar tw-flex tw-w-1/2 tw-items-center tw-justify-center tw-grow"
     >
-      <div class="menu-mobile-wrapper">
+      <div
+        class="menu-mobile-wrapper lg:tw-hidden"
+        :class="{
+          'tw-fixed tw-bg-black tw-z-30 tw-w-screen tw-h-screen':
+            menuDrawer.value === true,
+        }"
+      >
         <q-drawer side="left" v-model="menuDrawer" overlay>
           <q-btn
-            class="tw-absolute tw-right-2 tw-z-20"
+            class="tw-absolute tw-right-2 tw-top-2 tw-z-20"
             flat
             round
             dense
@@ -31,39 +37,32 @@
 
             <nav class="tw-p-4 tw-flex tw-flex-col">
               <q-list padding>
-                <q-item clickable v-ripple>
-                  <q-item-section> Página Inicial </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple>
-                  <q-item-section> Todos os Cursos </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple>
-                  <q-item-section> Materiais </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple>
-                  <q-item-section> Planos </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple>
-                  <q-item-section> Sobre Nós </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple>
-                  <q-item-section> Contato </q-item-section>
+                <q-item
+                  v-for="(item, index) in Menu"
+                  :key="`menu-mobile-${index}`"
+                  clickable
+                  v-ripple
+                  :to="item.link"
+                >
+                  <q-item-section> {{ item.label }} </q-item-section>
                 </q-item>
               </q-list>
             </nav>
           </q-scroll-area>
         </q-drawer>
 
-        <q-btn flat round dense icon="menu" @click="showMenu()" />
+        <q-btn flat icon="menu" label="MENU" @click="toggeMenu()" />
       </div>
 
       <nav class="tw-hidden lg:tw-flex">
-        <q-btn flat color="white" label="Página Inicial" />
-        <q-btn flat color="white" label="Todos os Cursos" />
-        <q-btn flat color="white" label="Materiais" />
-        <q-btn flat color="white" label="Planos" />
-        <q-btn flat color="white" label="Sobre Nós" />
-        <q-btn flat color="white" label="Contato" />
+        <q-btn
+          flat
+          :color="checkLinkIsActiveState(item.link) ? 'primary' : 'white'"
+          v-for="(item, index) in Menu"
+          :key="`menu-desktop-${index}`"
+          :label="item.label"
+          :to="item.link"
+        />
       </nav>
     </div>
     <div class="cta-group flex">
@@ -80,14 +79,34 @@
 </template>
 
 <script lang="ts" setup>
+import Menu from "@/services/menu";
+
+const route = useRoute();
+
 const menuDrawer = ref(false);
 
 function showMenu() {
   menuDrawer.value = true;
 }
 
+function toggeMenu() {
+  menuDrawer.value = !menuDrawer.value;
+}
+
 function hideMenu() {
   menuDrawer.value = false;
+}
+
+function clickOutOfMenu() {
+  hideMenu();
+}
+
+function checkLinkIsActiveState(link: string) {
+  if (route.fullPath !== link) {
+    return false;
+  }
+
+  return true;
 }
 </script>
 
---
+++ services/menu.ts
@@ -0,0 +1,26 @@
+export default [
+    {
+        label: 'Página Inicial',
+        link: '/'
+    },
+    {
+        label: 'Todos os Cursos',
+        link: '/cursos'
+    },
+    {
+        label: 'Materiais',
+        link: '/materiais'
+    },
+    {
+        label: 'Planos',
+        link: '/planos'
+    },
+    {
+        label: 'Sobre Nós',
+        link: '/sobre-nos'
+    },
+    {
+        label: 'Contato',
+        link: '/contato'
+    },
+]
