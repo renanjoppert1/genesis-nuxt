1e74bfad78df492ffd06d70e5b22c739c23f2106
Renan Joppert <renanjoppert@outlook.com>
2023-10-25 19:04:32 -0300

    Página de materiais

---
+++ components/Material/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <NuxtLink
+    to="/materiais/nome-do-ebook"
+    class="tw-bg-white/[.03] tw-rounded-md"
+  >
+    <div class="material-card">
+      <div class="tw-card">
+        <div class="thumbnail">
+          <img src="@/assets/img/ebook-thumbnail.jpg" />
+        </div>
+        <div class="ebook-content tw-box-border tw-p-4">
+          <h2 class="tw-text-base tw-mb-3 tw-text-genesis tw-font-bold">
+            Título 1 do Ebook
+          </h2>
+          <div>
+            <p>324 Páginas</p>
+            <p>Teologia</p>
+          </div>
+
+          <q-btn class="tw-w-full tw-mt-4 tw-flex" size="md" color="primary">
+            <AppIcon
+              class="tw-text-black tw-text-2xl"
+              name="ant-design:read-outlined"
+            />
+            <span class="tw-text-black tw-font-semibold tw-ml-2"
+              >Começar a Ler</span
+            >
+          </q-btn>
+        </div>
+      </div>
+    </div>
+  </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+</style>
+
--- pages/cursos.vue
+++ pages/cursos.vue
@@ -44,85 +44,8 @@
 </template>
 
 <script lang="ts" setup>
-interface CategorieOption {
-  id: number;
-  label: string;
-}
+import categoriesOptions from "@/services/api/categories";
 
-const categoriesOptions: CategorieOption[] = [
-  {
-    id: 1,
-    label: "Teologia",
-  },
-  {
-    id: 2,
-    label: "Intimidade com Deus",
-  },
-  {
-    id: 3,
-    label: "Evangelismo",
-  },
-  {
-    id: 1,
-    label: "Devocional",
-  },
-  {
-    id: 4,
-    label: "Ministração",
-  },
-  {
-    id: 5,
-    label: "Oratória",
-  },
-  {
-    id: 6,
-    label: "Comunicação",
-  },
-  {
-    id: 7,
-    label: "Homem",
-  },
-  {
-    id: 8,
-    label: "Mulher",
-  },
-  {
-    id: 9,
-    label: "Casados",
-  },
-  {
-    id: 10,
-    label: "Pais",
-  },
-  {
-    id: 11,
-    label: "Filhos",
-  },
-  {
-    id: 12,
-    label: "Relacionamento",
-  },
-  {
-    id: 13,
-    label: "Oração",
-  },
-  {
-    id: 14,
-    label: "Ministério de Louvor",
-  },
-  {
-    id: 15,
-    label: "Jejum",
-  },
-  {
-    id: 16,
-    label: "Ministério Infantil",
-  },
-  {
-    id: 17,
-    label: "Finanças Pessoais",
-  },
-];
 const searchOptions = ref(categoriesOptions);
 
 const searchCategorieModel = ref("");
