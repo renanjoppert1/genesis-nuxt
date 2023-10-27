f9ae7069a5ef7596b6a7ef63cc4057a3ac5a0425
Renan Joppert <renanjoppert@outlook.com>
2023-10-27 12:49:49 -0300

    Página sobre

---
+++ pages/sobre-nos.vue
@@ -0,0 +1,322 @@
+<template>
+  <section class="page-sobre-nos">
+    <AppContainer class="tw-flex-col tw-gap-10">
+      <div
+        class="tw-min-h-96 tw-relative tw-flex tw-flex-1 tw-shrink-0 tw-items-center tw-justify-center tw-overflow-hidden tw-rounded-lg tw-bg-gray-100 tw-py-16 tw-shadow-lg md:tw-py-20 xl:tw-py-48"
+      >
+        <!-- image - start -->
+        <img
+          src="https://images.unsplash.com/photo-1618004652321-13a63e576b80?auto=format&q=75&fit=crop&w=1500"
+          loading="lazy"
+          alt="Photo by Fakurian Design"
+          class="tw-absolute tw-inset-0 tw-h-full tw-w-full tw-object-cover tw-object-center"
+        />
+        <!-- image - end -->
+
+        <!-- overlay - start -->
+        <div
+          class="tw-absolute tw-inset-0 tw-bg-genesis tw-mix-blend-multiply"
+        ></div>
+        <!-- overlay - end -->
+
+        <!-- text start -->
+        <div
+          class="tw-relative tw-flex tw-flex-col tw-items-center tw-p-4 sm:tw-max-w-xl"
+        >
+          <p
+            class="tw-mb-4 tw-text-center tw-text-lg tw-text-indigo-50 sm:tw-text-xl md:tw-mb-8"
+          >
+            Uma Breve Introdução
+          </p>
+          <h1
+            class="tw-mb-8 tw-text-center tw-text-4xl tw-font-bold tw-text-white sm:tw-text-5xl md:tw-mb-12 md:tw-text-6xl"
+          >
+            Sobre Nós
+          </h1>
+
+          <div
+            class="tw-flex tw-w-full tw-flex-col tw-gap-2.5 sm:tw-flex-row sm:tw-justify-center"
+          >
+            <a
+              href="#"
+              class="tw-inline-block tw-rounded-lg tw-bg-black tw-px-8 tw-py-3 tw-text-center tw-text-sm tw-font-semibold tw-text-white tw-outline-none tw-ring-indigo-300 tw-transition tw-duration-100 focus-visible:tw-ring active:tw-bg-black md:tw-text-base"
+              >Nossa História</a
+            >
+
+            <a
+              href="#"
+              class="tw-inline-block tw-rounded-lg tw-bg-gray-200 tw-px-8 tw-py-3 tw-text-center tw-text-sm tw-font-semibold tw-text-gray-500 tw-outline-none tw-ring-indigo-300 tw-transition tw-duration-100 hover:tw-bg-gray-300 focus-visible:tw-ring active:tw-text-gray-700 md:tw-text-base"
+              >Nossa Equipe</a
+            >
+          </div>
+        </div>
+        <!-- text end -->
+      </div>
+
+      <div class="tw-text-gray-400 tw-body-font">
+        <div class="tw-container tw-px-5 tw-py-24 tw-mx-auto">
+          <div class="tw-flex tw-flex-col tw-text-center tw-w-full tw-mb-20">
+            <h2
+              class="tw-text-xs tw-text-genesis tw-tracking-widest tw-font-medium tw-title-font tw-mb-1"
+            >
+              Equipando Vidas, Inspirando Almas: Nossa Missão, Sua Jornada
+            </h2>
+            <h1
+              class="sm:tw-text-3xl text-tw-2xl tw-font-medium tw-title-font tw-mb-4 tw-text-white"
+            >
+              Um Hub de Crescimento Espiritual
+            </h1>
+            <p class="lg:tw-w-2/3 tw-mx-auto tw-leading-relaxed tw-text-base">
+              Fundamos pela paixão do Evangelho uma escola para equipar e
+              inspirar pessoas em sua jornada espiritual. Com fé sólida e amor
+              compassivo, promovemos a excelência, unidos em comunidade.
+            </p>
+          </div>
+          <div class="tw-flex tw-flex-wrap">
+            <div
+              class="xl:tw-w-1/4 lg:tw-w-1/2 md:tw-w-full tw-px-8 tw-py-6 tw-border-l-2 tw-border-gray-800"
+            >
+              <h2
+                class="tw-text-lg sm:tw-text-xl tw-text-white tw-font-medium tw-title-font tw-mb-2"
+              >
+                Nossa História
+              </h2>
+              <p class="tw-leading-relaxed tw-text-base tw-mb-4">
+                A Escola Genesis nasceu da paixão e dedicação de um grupo de
+                indivíduos inspirados pela mensagem transformadora do Evangelho.
+                Fundada em [ano de fundação], nossa jornada começou com a visão
+                de fornecer recursos valiosos para aqueles que buscam uma vida
+                cristã significativa. Desde então, temos crescido e evoluído,
+                continuamente dedicados a equipar e inspirar pessoas em sua
+                caminhada espiritual.
+              </p>
+            </div>
+            <div
+              class="xl:tw-w-1/4 lg:tw-w-1/2 md:tw-w-full tw-px-8 tw-py-6 tw-border-l-2 tw-border-gray-800"
+            >
+              <h2
+                class="tw-text-lg sm:tw-text-xl tw-text-white tw-font-medium tw-title-font tw-mb-2"
+              >
+                Nossa Missão
+              </h2>
+              <p class="tw-leading-relaxed tw-text-base tw-mb-4">
+                Capacitar e Inspirar - Essas duas palavras formam o cerne da
+                nossa missão na Escola Genesis. Estamos empenhados em capacitar
+                as pessoas com conhecimento sólido, baseado na Palavra de Deus,
+                por meio de cursos online, audiobooks, ebooks e blog posts que
+                abrangem uma ampla gama de tópicos cristãos.
+
+                <br /><br />Ao mesmo tempo, buscamos inspirar uma jornada
+                espiritual mais profunda e significativa, ajudando indivíduos a
+                crescer em sua fé, aprofundar seu relacionamento com Deus e
+                impactar positivamente suas comunidades.
+              </p>
+            </div>
+            <div
+              class="xl:tw-w-1/4 lg:tw-w-1/2 md:tw-w-full tw-px-8 tw-py-6 tw-border-l-2 tw-border-gray-800"
+            >
+              <h2
+                class="tw-text-lg sm:tw-text-xl tw-text-white tw-font-medium tw-title-font tw-mb-2"
+              >
+                Nossos Valores
+              </h2>
+              <p class="tw-leading-relaxed tw-text-base tw-mb-4"></p>
+              <ul
+                class="tw-mb-4 tw-flex tw-flex-col tw-gap-3 tw-list-disc tw-pl-4 tw-text-base"
+              >
+                <li>
+                  Fé Fundamentada: Acreditamos na importância de uma fé sólida,
+                  enraizada na Palavra de Deus.
+                </li>
+                <li>
+                  Amor e Compaixão: Nossa abordagem é guiada pelo amor e
+                  compaixão em todos os aspectos do nosso ministério
+                </li>
+                <li>
+                  Excelência: Buscamos a excelência em nossos recursos e
+                  serviços, refletindo a grandeza de Deus.
+                </li>
+                <li>
+                  Comunidade: Valorizamos o poder da comunidade e do
+                  compartilhamento de experiências na jornada da fé.
+                </li>
+              </ul>
+            </div>
+            <div
+              class="xl:tw-w-1/4 lg:tw-w-1/2 md:tw-w-full tw-px-8 tw-py-6 tw-border-l-2 tw-border-gray-800"
+            >
+              <h2
+                class="tw-text-lg sm:tw-text-xl tw-text-white tw-font-medium tw-title-font tw-mb-2"
+              >
+                Nos Comprometemos com Você!
+              </h2>
+              <p>
+                Na Escola Genesis, estamos comprometidos em ser um farol de luz,
+                oferecendo orientação e suporte para aqueles que buscam crescer
+                em sua fé cristã. Junte-se a nós nesta emocionante jornada
+                espiritual! <br /><br />
+
+                Se você deseja fazer parte da nossa comunidade ou precisa de
+                mais informações, não hesite em entrar em contato. Estamos aqui
+                para ajudar você em sua caminhada de fé.
+              </p>
+            </div>
+          </div>
+          <q-btn
+            color="primary"
+            class="tw-flex tw-mx-auto tw-mt-16 !tw-text-black tw-border-0 tw-py-2 tw-px-8 focus:tw-outline-none tw-rounded tw-text-lg tw-font-semibold"
+          >
+            Inicie Sua Jornada Agora Mesmo
+          </q-btn>
+        </div>
+      </div>
+
+      <div class="tw-w-full">
+        <p class="tw-text-center tw-text-3xl tw-font-semibold tw-text-white">
+          Podemos Dividir os Estágios de Aprendizado Em
+        </p>
+      </div>
+
+      <div class="tw-text-gray-400 tw-body-font">
+        <div
+          class="tw-container tw-px-5 tw-py-24 tw-mx-auto tw-flex tw-flex-wrap"
+        >
+          <div class="tw-flex tw-flex-wrap tw-w-full">
+            <div class="lg:tw-w-2/5 md:tw-w-1/2 md:tw-pr-10 md:tw-py-6">
+              <div class="tw-flex tw-relative tw-pb-12">
+                <div
+                  class="tw-h-full tw-w-10 tw-absolute tw-inset-0 tw-flex tw-items-center tw-justify-center"
+                >
+                  <div
+                    class="tw-h-full tw-w-1 tw-bg-gray-800 tw-pointer-events-none"
+                  ></div>
+                </div>
+                <div
+                  class="tw-flex-shrink-0 tw-w-10 tw-h-10 tw-rounded-full tw-bg-genesis tw-inline-flex tw-items-center tw-justify-center tw-text-black tw-relative tw-z-10"
+                >
+                  <AppIcon name="ph:cross-light" />
+                </div>
+                <div class="tw-flex-grow tw-pl-4">
+                  <h2
+                    class="tw-font-medium tw-title-font tw-text-sm tw-text-white tw-mb-1 tw-tracking-wider"
+                  >
+                    Conversão e Início
+                  </h2>
+                  <p class="tw-leading-relaxed">
+                    O início da jornada, com a decisão de aceitar a Cristo como
+                    Salvador.
+                  </p>
+                </div>
+              </div>
+              <div class="tw-flex tw-relative tw-pb-12">
+                <div
+                  class="tw-h-full tw-w-10 tw-absolute tw-inset-0 tw-flex tw-items-center tw-justify-center"
+                >
+                  <div
+                    class="tw-h-full tw-w-1 tw-bg-gray-800 tw-pointer-events-none"
+                  ></div>
+                </div>
+                <div
+                  class="tw-flex-shrink-0 tw-w-10 tw-h-10 tw-rounded-full tw-bg-genesis tw-inline-flex tw-items-center tw-justify-center tw-text-black tw-relative tw-z-10"
+                >
+                  <AppIcon name="ph:cross-light" />
+                </div>
+                <div class="tw-flex-grow tw-pl-4">
+                  <h2
+                    class="tw-font-medium tw-title-font tw-text-sm tw-text-white tw-mb-1 tw-tracking-wider"
+                  >
+                    Crescimento e Discipulado:
+                  </h2>
+                  <p class="tw-leading-relaxed">
+                    Crescimento na fé por meio do estudo da Bíblia, oração e
+                    envolvimento na igreja.
+                  </p>
+                </div>
+              </div>
+              <div class="tw-flex tw-relative tw-pb-12">
+                <div
+                  class="tw-h-full tw-w-10 tw-absolute tw-inset-0 tw-flex tw-items-center tw-justify-center"
+                >
+                  <div
+                    class="tw-h-full tw-w-1 tw-bg-gray-800 tw-pointer-events-none"
+                  ></div>
+                </div>
+                <div
+                  class="tw-flex-shrink-0 tw-w-10 tw-h-10 tw-rounded-full tw-bg-genesis tw-inline-flex tw-items-center tw-justify-center tw-text-black tw-relative tw-z-10"
+                >
+                  <AppIcon name="ph:cross-light" />
+                </div>
+                <div class="tw-flex-grow tw-pl-4">
+                  <h2
+                    class="tw-font-medium tw-title-font tw-text-sm tw-text-white tw-mb-1 tw-tracking-wider"
+                  >
+                    Ministério e Serviço
+                  </h2>
+                  <p class="tw-leading-relaxed">
+                    O desenvolvimento de habilidades e o serviço à comunidade
+                    cristã e além.
+                  </p>
+                </div>
+              </div>
+              <div class="tw-flex tw-relative tw-pb-12">
+                <div
+                  class="tw-h-full tw-w-10 tw-absolute tw-inset-0 tw-flex tw-items-center tw-justify-center"
+                >
+                  <div
+                    class="tw-h-full tw-w-1 tw-bg-gray-800 tw-pointer-events-none"
+                  ></div>
+                </div>
+                <div
+                  class="tw-flex-shrink-0 tw-w-10 tw-h-10 tw-rounded-full tw-bg-genesis tw-inline-flex tw-items-center tw-justify-center tw-text-black tw-relative tw-z-10"
+                >
+                  <AppIcon name="ph:cross-light" />
+                </div>
+                <div class="tw-flex-grow tw-pl-4">
+                  <h2
+                    class="tw-font-medium tw-title-font tw-text-sm tw-text-white tw-mb-1 tw-tracking-wider"
+                  >
+                    Maturidade Espiritual:
+                  </h2>
+                  <p class="tw-leading-relaxed">
+                    Um profundo relacionamento com Deus e a capacidade de guiar
+                    outros na fé.
+                  </p>
+                </div>
+              </div>
+              <div class="tw-flex tw-relative tw-pb-12">
+                <div
+                  class="tw-flex-shrink-0 tw-w-10 tw-h-10 tw-rounded-full tw-bg-genesis tw-inline-flex tw-items-center tw-justify-center tw-text-black tw-relative tw-z-10"
+                >
+                  <AppIcon name="ph:cross-light" />
+                </div>
+                <div class="tw-flex-grow tw-pl-4">
+                  <h2
+                    class="tw-font-medium tw-title-font tw-text-sm tw-text-white tw-mb-1 tw-tracking-wider"
+                  >
+                    Missão e Testemunho
+                  </h2>
+                  <p class="tw-leading-relaxed">
+                    Compartilhar a fé e fazer discípulos, cumprindo a Grande
+                    Comissão.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <img
+              class="lg:tw-w-3/5 md:tw-w-1/2 tw-object-cover tw-object-center tw-rounded-lg md:tw-mt-0 tw-mt-12"
+              src="@/assets/img/bg-steps.jpg"
+              alt="step"
+            />
+          </div>
+        </div>
+      </div>
+    </AppContainer>
+  </section>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
+
