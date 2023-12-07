57d6b93b449153426f3cd971dfdfc30742da7a86
Renan Joppert <renanljp@gmail.com>
2023-12-07 15:40:50 -0300

    página de termos de uso

---
+++ pages/termos-de-uso.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="page-termos-de-uso">
+    <AppContainer class="tw-flex-col tw-gap-2">
+      <AppPageTitle title="Termos de Uso" />
+
+      <h2>Dados da Empresa:</h2>
+      <p>Razão Social: Software Way</p>
+      <p>Endereço: Rua Nelson, 60 - Santa Quitéria - Curitiba/PR</p>
+
+      <h2>Descrição do Serviço:</h2>
+      <p>
+        O Software Way oferece planos mensais para acesso a conteúdos de cursos
+        em vídeo e em PDFs.
+      </p>
+
+      <h2>Pagamento</h2>
+      <p>
+        O pagamento dos planos será realizado através de assinatura,
+        exclusivamente via cartão de crédito, utilizando o gateway de pagamento
+        Asaas.
+      </p>
+
+      <h2>Entrega</h2>
+      <p>
+        A entrega dos conteúdos será realizada de forma digital e automática
+        logo após a confirmação do pagamento.
+      </p>
+
+      <h2>Período de Teste</h2>
+      <p>O cliente terá um prazo inicial de 30 dias após o pagamento para testar a ferramenta. Por lei, ele tem até 7 dias após o pagamento para solicitar a devolução completa do valor.</p>
+
+      <h2>Política de Privacidade</h2>
+      <p>Os dados coletados estarão em posse da Software Way e poderão ser compartilhados com parceiros como Google, Facebook e Microsoft, além de serem armazenados em serviços da AWS (Amazon Web Services).</p>
+
+      <h2>Condições de Uso</h2>
+      <p>O uso do site está sujeito às seguintes condições:</p>
+      <ul>
+        <li>O cliente concorda em não compartilhar conteúdos para terceiros.</li>
+        <li>Todo o material do site é de responsabilidade e propriedade exclusiva da Software Way.</li>
+      </ul>
+
+      <h2>Responsabilidade do Usuário</h2>
+      <p>O cliente é responsável por manter suas credenciais de acesso seguras e por qualquer atividade que ocorra sob sua conta.</p>
+
+      <h2>Propriedade Intelectual</h2>
+      <p>Todo o material disponível no site, incluindo cursos em vídeo e em PDFs, é de propriedade exclusiva da Software Way. Não são autorizados compartilhamentos de conteúdos para terceiros sem autorização expressa.</p>
+
+      <p>Ao utilizar os serviços da Software Way, o cliente concorda e aceita todos os termos e condições mencionados acima. A Software Way reserva-se o direito de modificar estes termos a qualquer momento, mediante aviso prévio aos usuários.</p>
+    </AppContainer>
+  </section>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
