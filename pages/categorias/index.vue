2788a5384faeb27d61c192a9436e9c01f6cdc87c
Renan Joppert <renanljp@gmail.com>
2023-12-08 11:58:28 -0300

    Página de categorias

---
+++ pages/categorias/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="page_materiais">
+    <AppContainer class="tw-flex-col tw-gap-10">
+      <AppPageTitle title="Teologia" />
+
+      <div class="categories-list tw-w-full">
+        <div
+          class="ebook-list tw-flex tw-flex-wrap tw-gap-10 tw-mt-10 tw-justify-center md:tw-justify-start"
+        >
+          <MaterialCard />
+          <MaterialCard />
+          <MaterialCard />
+          <MaterialCard />
+        </div>
+      </div>
+    </AppContainer>
+  </section>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+</style>
+
