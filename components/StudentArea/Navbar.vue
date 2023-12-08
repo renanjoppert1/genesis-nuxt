5d32b25013431646561d146a8902560d5da1fe36
Renan Joppert <renanljp@gmail.com>
2023-12-08 01:45:49 -0300

    Página de perfil do aluno

---
+++ components/StudentArea/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tw-w-full tw-flex tw-gap-5">
+    <q-btn
+      color="primary"
+      label="Meu Perfil"
+      class="!tw-text-black tw-text-base"
+      to="/area-do-aluno/perfil"
+    />
+
+    <q-btn
+      color="primary"
+      label="Minha Senha"
+      class="!tw-text-primary tw-text-base"
+      outline
+      to="/area-do-aluno/alteracao-senha"
+    />
+
+    <q-btn
+      color="primary"
+      label="Assinatura e Recibos"
+      class="!tw-text-primary tw-text-base"
+      outline
+      to="/area-do-aluno/assinatura"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+</style>
