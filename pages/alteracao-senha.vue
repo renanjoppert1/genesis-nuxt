1d0f7af0fc40dca0103e6d69a60cc9b63945471b
Renan Joppert <renanljp@gmail.com>
2023-12-07 21:55:14 -0300

    Modelagem de alteração de senha e recuperação de senha

--- pages/alteracao-senha.vue
+++ pages/alteracao-senha.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="page__login">
+  <section class="page__alteracao-senha">
     <AppContainer class="tw-flex-col tw-gap-10">
       <div class="tw-py-6 sm:tw-tw-py-8 lg:tw-tw-py-12">
         <div class="tw-mx-auto tw-max-w-screen-2xl tw-px-4 md:tw-tw-px-8">
@@ -7,18 +7,14 @@
             <h2
               class="tw-mb-4 tw-text-center tw-text-2xl tw-font-bold md:tw-tw-mb-8 lg:tw-tw-text-3xl tw-pt-8"
             >
-              Login
+              Alteração de Senha
             </h2>
-            <div class="tw-flex tw-flex-col tw-gap-4 tw-p-4 md:tw-p-8">
-              <div>
-                <q-input
-                  class="tw-col-span-2"
-                  filled
-                  v-model="email"
-                  label="E-mail"
-                />
-              </div>
 
+            <p class="tw-w-full tw-px-8">
+              Digite abaixo a sua nova senha e clique em alterar.
+            </p>
+
+            <div class="tw-flex tw-flex-col tw-gap-4 tw-p-4 md:tw-p-8">
               <div>
                 <q-input
                   class="tw-col-span-2"
@@ -38,32 +34,29 @@
               </div>
 
               <div>
-                <p>
-                  <NuxtLink to="esqueci-minha-senha" class="tw-text-sm"
-                    >Esqueceu sua senha?</NuxtLink
-                  >
-                </p>
+                <q-input
+                  class="tw-col-span-2"
+                  v-model="confirmPassword"
+                  filled
+                  :type="isPwd ? 'password' : 'text'"
+                  label="Confirmar Senha"
+                >
+                  <template v-slot:append>
+                    <q-icon
+                      :name="isPwd ? 'visibility_off' : 'visibility'"
+                      class="cursor-pointer"
+                      @click="isPwd = !isPwd"
+                    />
+                  </template>
+                </q-input>
               </div>
 
               <q-btn
                 color="primary"
                 class="tw-block tw-bg-gray-800 tw-px-8 tw-py-3 tw-text-center tw-text-sm tw-font-semibold !tw-text-black tw-outline-none tw-ring-gray-300 tw-transition tw-duration-100 tw-hover:tw-bg-gray-700 tw-focus-visible:tw-ring tw-active:tw-bg-gray-600 md:tw-text-base"
               >
-                Entrar
+                Alterar Senha
               </q-btn>
-            </div>
-
-            <div
-              class="tw-flex tw-items-center tw-justify-center tw-bg-white/[0.05] tw-p-4"
-            >
-              <p class="tw-text-center tw-text-sm tw-text-gray-500">
-                Não Possui Uma Conta?
-                <NuxtLink
-                  to="/periodo-de-avaliacao"
-                  class="tw-text-genesis tw-transition tw-duration-100 tw-hover:tw-text-indigo-600 tw-active:tw-text-indigo-700"
-                  >Cadastre-se Gratuitamente</NuxtLink
-                >
-              </p>
             </div>
           </form>
         </div>
--- pages/esqueci-minha-senha.vue
+++ pages/esqueci-minha-senha.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="page__login">
+  <section class="page__esqueci-minha-senha">
     <AppContainer class="tw-flex-col tw-gap-10">
       <div class="tw-py-6 sm:tw-tw-py-8 lg:tw-tw-py-12">
         <div class="tw-mx-auto tw-max-w-screen-2xl tw-px-4 md:tw-tw-px-8">
@@ -7,8 +7,12 @@
             <h2
               class="tw-mb-4 tw-text-center tw-text-2xl tw-font-bold md:tw-tw-mb-8 lg:tw-tw-text-3xl tw-pt-8"
             >
-              Login
+              Recuperação de Senha
             </h2>
+            <p class="tw-w-full tw-px-8">
+              Digite seu e-mail abaixo e clique em enviar e você receberá um
+              link de redefinição de senha na sua caixa de e-mail.
+            </p>
             <div class="tw-flex tw-flex-col tw-gap-4 tw-p-4 md:tw-p-8">
               <div>
                 <q-input
@@ -19,38 +23,23 @@
                 />
               </div>
 
-              <div>
-                <q-input
-                  class="tw-col-span-2"
-                  v-model="password"
-                  filled
-                  :type="isPwd ? 'password' : 'text'"
-                  label="Senha"
-                >
-                  <template v-slot:append>
-                    <q-icon
-                      :name="isPwd ? 'visibility_off' : 'visibility'"
-                      class="cursor-pointer"
-                      @click="isPwd = !isPwd"
-                    />
-                  </template>
-                </q-input>
-              </div>
-
-              <div>
-                <p>
-                  <NuxtLink to="esqueci-minha-senha" class="tw-text-sm"
-                    >Esqueceu sua senha?</NuxtLink
-                  >
-                </p>
-              </div>
-
               <q-btn
                 color="primary"
                 class="tw-block tw-bg-gray-800 tw-px-8 tw-py-3 tw-text-center tw-text-sm tw-font-semibold !tw-text-black tw-outline-none tw-ring-gray-300 tw-transition tw-duration-100 tw-hover:tw-bg-gray-700 tw-focus-visible:tw-ring tw-active:tw-bg-gray-600 md:tw-text-base"
               >
-                Entrar
+                Enviar
               </q-btn>
+
+              <div>
+                <p>
+                  <NuxtLink
+                    to="/login"
+                    class="tw-text-sm tw-flex tw-items-center tw-gap-3 tw-justify-center"
+                    ><AppIcon name="pajamas:go-back" /> Voltar para a página de
+                    login</NuxtLink
+                  >
+                </p>
+              </div>
             </div>
 
             <div
