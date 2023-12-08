70a81bc96eff280fd31221f76ea3610f64b60f22
Renan Joppert <renanljp@gmail.com>
2023-12-08 01:59:24 -0300

    Ajuste na navbar do aluno

--- components/StudentArea/Navbar.vue
+++ components/StudentArea/Navbar.vue
@@ -1,31 +1,49 @@
 <template>
   <div class="tw-w-full tw-flex tw-gap-5">
     <q-btn
+      v-for="btn in links"
+      :key="btn.link"
       color="primary"
-      label="Meu Perfil"
-      class="!tw-text-black tw-text-base"
-      to="/area-do-aluno/perfil"
-    />
-
-    <q-btn
-      color="primary"
-      label="Minha Senha"
-      class="!tw-text-primary tw-text-base"
-      outline
-      to="/area-do-aluno/alteracao-senha"
-    />
-
-    <q-btn
-      color="primary"
-      label="Assinatura e Recibos"
-      class="!tw-text-primary tw-text-base"
-      outline
-      to="/area-do-aluno/assinatura"
+      :label="btn.label"
+      :outline="isCurrentPage(btn.link) == false"
+      class="tw-text-black tw-text-base"
+      :class="{
+        'tw-text-primary': isCurrentPage(btn.link) === false,
+        '!tw-text-black': isCurrentPage(btn.link),
+      }"
+      :to="btn.link"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
+const route = useRoute();
+const links = ref([
+  {
+    label: "Meu Perfil",
+    link: "/area-do-aluno/perfil",
+  },
+  {
+    label: "Minha Senha",
+    link: "/area-do-aluno/alteracao-senha",
+  },
+  {
+    label: "Assinatura e Recibos",
+    link: "/area-do-aluno/assinatura",
+  },
+  {
+    label: "Certificados",
+    link: "/area-do-aluno/certificados",
+  },
+]);
+
+function isCurrentPage(link: string) {
+  if (route.path.includes(link)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 </script>
 
 <style>
---
+++ pages/area-do-aluno/assinatura.vue
@@ -0,0 +1,115 @@
+<template>
+  <section class="page__dashboard">
+    <AppContainer class="tw-flex-col tw-gap-10">
+      <AppPageTitle title="Meu Perfil" />
+
+      <StudentAreaNavbar />
+
+      <div
+        class="tw-bg-white/[0.05] tw-rounded-md tw-w-full tw-p-4 tw-text-base"
+      >
+        <form>
+          <p class="tw-font-semibold tw-text-xl tw-my-6">Dados do Perfil</p>
+
+          <div
+            class="tw-w-full tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-5"
+          >
+            <div>
+              <q-input
+                class="tw-col-span-2"
+                filled
+                v-model="name"
+                label="Nome Completo"
+              />
+            </div>
+            <div>
+              <q-input
+                class="tw-col-span-2"
+                filled
+                v-model="birthdate"
+                label="Data de Nascimento"
+              />
+            </div>
+            <div>
+              <q-input
+                class="tw-col-span-2"
+                filled
+                v-model="email"
+                label="E-mail"
+              />
+            </div>
+            <div>
+              <q-input
+                class="tw-col-span-2"
+                filled
+                v-model="genero"
+                label="Gênero"
+              />
+            </div>
+          </div>
+
+          <p class="tw-font-semibold tw-text-xl tw-my-8">Dados de Endereço</p>
+
+          <div
+            class="tw-w-full tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-5"
+          >
+            <div>
+              <q-input class="tw-col-span-2" filled v-model="cep" label="CEP" />
+            </div>
+            <div>
+              <q-input
+                class="tw-col-span-2"
+                filled
+                v-model="address"
+                label="Endereço"
+              />
+            </div>
+            <div>
+              <q-input
+                class="tw-col-span-2"
+                filled
+                v-model="neighborhood"
+                label="Bairro"
+              />
+            </div>
+            <div>
+              <q-input
+                class="tw-col-span-2"
+                filled
+                v-model="city"
+                label="Cidade"
+              />
+            </div>
+            <div>
+              <q-input
+                class="tw-col-span-2"
+                filled
+                v-model="state"
+                label="Estado"
+              />
+            </div>
+          </div>
+          <q-btn
+            color="primary"
+            label="Salvar"
+            class="!tw-text-black tw-text-base tw-mt-8"
+          />
+        </form>
+      </div>
+    </AppContainer>
+  </section>
+</template>
+
+<script lang="ts" setup>
+const categories = ref({
+  iconSize: "24px",
+  color: "primary",
+  padding: "1.5em",
+  textColor: "primary",
+  classTextSize: "tw-text-base",
+  outline: true,
+});
+</script>
+
+<style>
+</style>
